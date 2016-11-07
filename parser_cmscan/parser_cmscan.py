bd3493cdae39184b4beef4cb777545395f7f8353
nataquinones <nataquinones@gmail.com>
2016-11-07 12:07:13 +0000

    Add e_value to parser, update readme, update sample output file.

--- parser_cmscan/parser_cmscan.py
+++ parser_cmscan/parser_cmscan.py
@@ -12,8 +12,8 @@ df = pd.read_table(
 	header=None
 	)
 
-df = df[[3,2,1,5,19]] 
-df.columns = ['id', 'hit_rfam_acc', 'fam_name', 'hit_clan_acc', 'olp']
+df = df[[3,2,1,5,19, 17]] 
+df.columns = ['id', 'hit_rfam_acc', 'fam_name', 'hit_clan_acc', 'olp', 'e_value']
 df = df.loc[df['olp'] != '=']
 
 df.to_csv(outputfile, sep='\t', index = False)
