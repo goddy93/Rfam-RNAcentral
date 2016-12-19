3bd024ebeeaaf416ae7084c75e85a28d3ae65c81
nataquinones <nataquinones@gmail.com>
2016-12-19 11:18:54 +0000

    Two python scripts to load tables into the mysql database

---
+++ database/load_id.py
@@ -0,0 +1,9 @@
+import sqlalchemy
+import os
+
+files = os.listdir(".")
+
+engine = sqlalchemy.create_engine('mysql+mysqldb://root:rna@localhost/rnac_rfam')
+connection = engine.connect()
+for file in files:
+	connection.execute("load data local infile '%s' into table cmscan_run ignore 1 lines;" % file)
