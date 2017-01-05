29eab470ae2214b65983391c663603f2c839d767
nataquinones <nataquinones@gmail.com>
2017-01-05 15:39:27 +0000

    Get table length from actual results instead of from total count. Avoids table length error when more than 50 refs. Inserts line indicating 50+ if is the case.

--- new_fams/02.get_fasta.py
+++ new_fams/02.get_fasta.py
@@ -8,30 +8,32 @@ pseudonewfam_fasta_out = "./files/pseudonewfam.fasta"
 
 # read full new family file
 df_eunewfam = pd.read_table(
-   eunewfam_in,
-    low_memory=False,
-    sep="\t"
-    )
+  eunewfam_in,
+  low_memory=False,
+  sep="\t"
+  )
 
 df_pseudonewfam = pd.read_table(
-   pseudonewfam_in,
-    low_memory=False,
-    sep="\t"
-    )
+  pseudonewfam_in,
+  low_memory=False,
+  sep="\t"
+  )
 
 # make lists with urs
 eunewfam_list = df_eunewfam["id"].tolist()
 pseudonewfam_list = df_pseudonewfam["id"].tolist()
 
 # make FASTA file with all the sequences in a list
+
+
 def fasta_seq(urs_list, outfile):
-	f = open(outfile,"w")
-	for i in urs_list:
-		url = "http://rnacentral.org/api/v1/rna/"+i+"?format=fasta"
-		r = requests.get(url)
-		f.write(r.content)
-	f.close()
+    f = open(outfile, "w")
+    for i in urs_list:
+        url = "http://rnacentral.org/api/v1/rna/"+i+"?format=fasta"
+        r = requests.get(url, timeout=10)
+        f.write(r.content)
+    f.close()
 
 # run function for both lists
-fasta_seq(eunewfam_list[0:10000], eunewfam_fasta_out)
-#fasta_seq(pseudonewfam_list, pseudonewfam_fasta_out)
+fasta_seq(eunewfam_list, eunewfam_fasta_out)
+# fasta_seq(pseudonewfam_list, pseudonewfam_fasta_out)
--- new_fams/03.get_pub.py
+++ new_fams/03.get_pub.py
@@ -8,46 +8,52 @@ pseudonewfam_pub_out = "./files/df_pseudonewfam.pub"
 
 # read full new family file
 df_eunewfam = pd.read_table(
-   eunewfam_in,
-    low_memory=False,
-    sep="\t"
-    )
+  eunewfam_in,
+  low_memory=False,
+  sep="\t"
+  )
 
 df_pseudonewfam = pd.read_table(
-   pseudonewfam_in,
-    low_memory=False,
-    sep="\t"
-    )
+  pseudonewfam_in,
+  low_memory=False,
+  sep="\t"
+  )
 
 # make lists with urs
 eunewfam_list = df_eunewfam["id"].tolist()
 pseudonewfam_list = df_pseudonewfam["id"].tolist()
 
 # make FASTA file with all the sequences in a list
+
+
 def pubmed_id(urs_list):
-  df2 = pd.DataFrame()
-  for i in urs_list:
-    pubmed_ids = []
-    pub_ids = []
-    url = "http://rnacentral.org/api/v1/rna/"+i+"/publications?page_size=100"
-    r = requests.get(url)
-    data = r.json()
-    a = data["count"]
-    # make df
-    urss = [i] * a
-    for j in range (0,a):
-      pubmed_ids.append(data['results'][j]["pubmed_id"])
-    for j in range (0,a):
-      pub_ids.append(data['results'][j]["pub_id"])
-    df = pd.DataFrame(urss)
-    df.columns = ["URS"]
-    df["pubmed_id"] = pubmed_ids
-    df["pub_id"] = pub_ids
-    df2 = df2.append(df)
-    df2.reset_index(drop=True, inplace=True)
-  return df2
+    df2 = pd.DataFrame()
+    for i in urs_list:
+        pubmed_ids = []
+        pub_ids = []
+        url = "http://rnacentral.org/api/v1/rna/"+i+"/publications?page_size=50"
+        r = requests.get(url, timeout=10)
+        data = r.json()
+        a = len(data["results"])
+        # make df
+        urss = [i] * a
+        for j in range(0, a):
+            pubmed_ids.append(data['results'][j]["pubmed_id"])
+        for j in range(0, a):
+            pub_ids.append(data['results'][j]["pub_id"])
+        df = pd.DataFrame(urss)
+        df.columns = ["URS"]
+        df["pubmed_id"] = pubmed_ids
+        df["pub_id"] = pub_ids
+        df2 = df2.append(df)
+        # check if count
+        if a != data["count"]:
+          df2.loc[-1] = [i, "50+", "50+"]
+        # reset index
+        df2.reset_index(drop=True, inplace=True)
+    return df2
 
 # run function for both lists and save tables to csvs
-df_pub = pubmed_id(eunewfam_list[0:1000])
+df_pub = pubmed_id(eunewfam_list)
 df_pub.to_csv(eunewfam_pub_out, sep="\t", na_rep="None", header=True, index=False)
 #pubmed_id(pseudonewfam_list).to_csv(pseudonewfam_pub_out, sep="\t", na_rep="None", header=True, index=False)
