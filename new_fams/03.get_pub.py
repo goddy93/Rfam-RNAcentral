89a8643e2cc707abf340ba73e4bc7c18c0977790
nataquinones <nataquinones@gmail.com>
2017-01-06 14:33:10 +0000

    PEP8

--- new_fams/03.get_pub.py
+++ new_fams/03.get_pub.py
@@ -1,39 +1,61 @@
 import pandas as pd
 import requests
 
-eunewfam_in = "./files/df_eunewfam"
-eunewfam_pub_out = "./files/df_eunewfam.pub"
-
-# read full new family file
-df_eunewfam = pd.read_table(
-  eunewfam_in,
-  low_memory=False,
-  sep="\t"
-  )
-
-# make lists with urs
-eunewfam_list = df_eunewfam["id"].tolist()
+EUNEWFAM_IN = "./files/df_eunewfam"  # input file
+EUNEWFAM_PUB_OUT = "./files/df_eunewfam.pub"  # output file
 
 
 def pubmed_id(urs_list, page_size, time_out):
-    f = open(eunewfam_pub_out, "w")
+    """
+    Function that creates tab delimited file with publication ids
+    (pubmed_id and pub_id) per reference, for a list of RNAcentral URSs.
+    """
+    out_file = open(EUNEWFAM_PUB_OUT, "w")
     # write header
-    f.write("URS\tpubmed_id\tpub_id\n")
-    # search for each
+    out_file.write("URS\tpubmed_id\tpub_id\n")
+    # request from RNAcentral, write into file or indicate error
     for i in urs_list:
-        url = "http://rnacentral.org/api/v1/rna/"+i+"/publications?page_size="+str(page_size)
+        url = ("http://rnacentral.org/api/v1/rna/" +
+               i +
+               "/publications?page_size=" +
+               str(page_size))
+        # write into file (show err if timeout, or results exceed page length)
         try:
-            r = requests.get(url, timeout=time_out)
-            data = r.json()
-            a = len(data["results"])
-            for j in range(0, a):
+            req = requests.get(url, timeout=time_out)
+            data = req.json()
+            num_ref = len(data["results"])
+            for j in range(0, num_ref):
                 pubmed = str(data['results'][j]["pubmed_id"])
                 pubid = str(data['results'][j]["pub_id"])
-                f.write(i+"\t"+pubmed+"\t"+pubid+"\n")
-            if a != data["count"]:
-                f.write(i+"\t"+"err("+str(page_size)+"+)"+"\t"+"err("+str(page_size)+"+)"+"\n")
+                out_file.write(i + "\t" +
+                               pubmed +
+                               "\t" +
+                               pubid +
+                               "\n")
+            if num_ref != data["count"]:
+                out_file.write(i +
+                               "\terr(" +
+                               str(page_size) +
+                               "+)\terr(" +
+                               str(page_size) +
+                               "+)\n")
         except requests.exceptions.Timeout:
-            f.write(i+"\terr(timeout)\terr(timeout)\n")
-    f.close()
+            out_file.write(i+"\terr(timeout)\terr(timeout)\n")
+    out_file.close()
 
-pubmed_id(eunewfam_list, 50, 10)
+# read input table
+DF_EUNEWFAM = pd.read_table(
+    EUNEWFAM_IN,
+    low_memory=False,
+    sep="\t"
+    )
+
+# make list from id column
+EUNEWFAM_LIST = DF_EUNEWFAM["id"].tolist()
+EUNEWFAM_LIST = EUNEWFAM_LIST[0:10]
+
+# input function args
+PAGE_LENGTH = 50  # results to be checked from RNAcentral API, max 100
+TIME_OUT_IN = 10  # in seconds
+
+pubmed_id(EUNEWFAM_LIST, PAGE_LENGTH, TIME_OUT_IN)
